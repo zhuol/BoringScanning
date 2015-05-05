89adfd91e200c3634f37d589289571e50a75ec77
root <root@ZHUbuntu14.spidynamics.com>
2015-05-05 12:33:51 -0400

    add socket

--- portScan.py
+++ portScan.py
@@ -1,14 +1,18 @@
 #! /usr/bin python
 # -*- coding: utf-8 -*-
 
-hosts = ['127.0.0.1']
+import socket
+
+hosts = ['127.0.0.1', 'google.com']
 ports = [22, 445, 80, 443, 3389]
+s = socket.socket()
 
 for host in hosts:
 	for port in ports:
+	#for port in range(20, 100, 3):
 		try:
-			print "[+] Connecting to  "+host+"str(port)"
-			s.connect(host, port)
+			print "[+] Connecting to  "+host+":"+str(port)
+			s.connect((host, port))
 			s.send('Primal Security \n')		
 			banner = s.recv(1024)
 			if banner:
