33a203212bdd75493ad36c0610dbc8701ff62b3b
root <root@ZHUbuntu14.spidynamics.com>
2015-05-05 12:09:27 -0400

    Some old scan code

---
+++ portScan.py
@@ -0,0 +1,17 @@
+#! /usr/bin python
+
+hosts = ['127.0.0.1']
+ports = [22, 445, 80, 443, 3389]
+
+for host in hosts:
+	for port in ports:
+		try:
+			print "[+] Connecting to  "+host+"str(port)"
+			s.connect(host, port)
+			s.send('Primal Security \n')		
+			banner = s.recv(1024)
+			if banner:
+				print "[+] Port "+str(port)+" open: "+banner
+			s.close()
+		except:
+			pass
